5c5e5c6b0b95f5fa7cd8cfca4913b3b3c823cfe5
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-10-21 14:52:47 +0200

    create listA

---
+++ src/components/input.tsx
@@ -0,0 +1,7 @@
+// import { useState } from "react";
+
+// export function UserInput() {
+//   const [input, setInput] = useState(null);
+
+//   return <input type="text"/>;
+// }
---
+++ src/components/list.tsx
@@ -0,0 +1,14 @@
+const people = [
+  { id: 0, name: "Carol" },
+  { id: 1, name: "Marcos" },
+  { id: 2, name: "Odin" },
+  { id: 3, name: "Lucas" },
+  { id: 4, name: "Mat" },
+];
+
+export function List() {
+  const listItems = people.map((person) => {
+    return <li>{person.name}</li>;
+  });
+  return <ul>{listItems}</ul>;
+}
